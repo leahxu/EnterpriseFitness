7e0f3823a18251cd836828275ed350e2b6ce060e
Leah Xu <t-leaxu@microsoft.com>
2014-07-23 14:18:29 -0700

    Writes to ActivizeDB database with DBWriterBolt

--- storm-starter/src/storm/starter/ActivizeTopology.java
+++ storm-starter/src/storm/starter/ActivizeTopology.java
@@ -1,136 +1,26 @@
 package storm.starter;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Properties;
 
-import storm.starter.bolt.MessageReceiverBolt;
 import storm.starter.bolt.CompanyAggregatorBolt;
+import storm.starter.bolt.DBWriterBolt;
+import storm.starter.bolt.FileWriterBolt;
+import storm.starter.bolt.MessageReceiverBolt;
 import storm.starter.bolt.UserAggregatorBolt;
 import storm.starter.spout.ServiceBusQueueConnection;
 import storm.starter.spout.ServiceBusQueueSpout;
 import storm.starter.spout.interfaces.IServiceBusQueueDetail;
 import backtype.storm.Config;
 import backtype.storm.LocalCluster;
-import backtype.storm.topology.BasicOutputCollector;
-import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.topology.TopologyBuilder;
-import backtype.storm.topology.base.BaseBasicBolt;
 import backtype.storm.tuple.Fields;
-import backtype.storm.tuple.Tuple;
-import backtype.storm.tuple.Values;
 
 public class ActivizeTopology {
 
-	public static File file;
-	public static FileWriter fw;
-	public static BufferedWriter bw;
-
-	public static File file2;
-	public static FileWriter fw2;
-	public static BufferedWriter bw2;
-
-	// writes to file deviceId.out
-	public static class DisplayBoltDevice extends BaseBasicBolt {
-		private static final long serialVersionUID = 1L;
-
-		@Override
-		public void execute(Tuple tuple, BasicOutputCollector collector) {
-			String deviceId = tuple.getString(0);
-			String companyId = tuple.getString(1);
-			String date = tuple.getString(2);
-			String time = tuple.getString(3);
-			double calorie = tuple.getDouble(4);
-			double distance = tuple.getDouble(5);
-			double runStep = tuple.getDouble(6);
-			double totalStep = tuple.getDouble(7);
-			double walkStep = tuple.getDouble(8);
-
-			try {
-				bw.write(deviceId + "|" + companyId + "|" + date + "|" + time
-						+ "|" + calorie + "|" + distance + "|" + runStep + "|"
-						+ totalStep + "|" + walkStep + "\n");
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-
-			collector.emit(new Values("Done"));
-		}
-
-		@Override
-		public void declareOutputFields(OutputFieldsDeclarer declarer) {
-			declarer.declare(new Fields("display"));
-		}
-	}
-
-	// writes to file company.out
-	public static class DisplayBoltCompany extends BaseBasicBolt {
-		private static final long serialVersionUID = 1L;
-
-		@Override
-		public void execute(Tuple tuple, BasicOutputCollector collector) {
-			String deviceId = tuple.getString(0);
-			String companyId = tuple.getString(1);
-			String date = tuple.getString(2);
-			String time = tuple.getString(3);
-			double calorie = tuple.getDouble(4);
-			double distance = tuple.getDouble(5);
-			double runStep = tuple.getDouble(6);
-			double totalStep = tuple.getDouble(7);
-			double walkStep = tuple.getDouble(8);
-
-			try {
-				bw2.write(deviceId + "|" + companyId + "|" + date + "|" + time
-						+ "|" + calorie + "|" + distance + "|" + runStep + "|"
-						+ totalStep + "|" + walkStep + "\n");
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-
-			collector.emit(new Values("Done"));
-		}
-
-		@Override
-		public void declareOutputFields(OutputFieldsDeclarer declarer) {
-			declarer.declare(new Fields("display"));
-		}
-	}
-
 	public static void main(String[] args) throws Exception {
-
-		// Creating a file to write the data to
-		// Temporary and for testing purposes
-		try {
-			file = new File("/home/leah/git/EnterpriseFitness/storm-starter/user.out");
-
-			if (!file.exists()) {
-				file.createNewFile();
-			}
-
-			fw = new FileWriter(file.getAbsoluteFile());
-			bw = new BufferedWriter(fw);
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		try {
-			file2 = new File("/home/leah/git/EnterpriseFitness/storm-starter/company.out");
-
-			if (!file2.exists()) {
-				file2.createNewFile();
-			}
-
-			fw2 = new FileWriter(file2.getAbsoluteFile());
-			bw2 = new BufferedWriter(fw2);
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 
 		Properties prop = new Properties();
 		try {
@@ -148,18 +38,22 @@ public class ActivizeTopology {
 				connectionString, queueName);
 		TopologyBuilder builder = new TopologyBuilder();
 
+		// sets spout, connects to ServiceBus Queue
 		builder.setSpout(spoutId, new ServiceBusQueueSpout(connection), 6);
 
+		// sets bolts
 		builder.setBolt("MessageReceiverBolt", new MessageReceiverBolt(), 3)
 				.shuffleGrouping(spoutId);
 		builder.setBolt("UserAggregatorBolt", new UserAggregatorBolt(), 3)
 				.fieldsGrouping("MessageReceiverBolt", new Fields("deviceId"));
 		builder.setBolt("CompanyAggregatorBolt", new CompanyAggregatorBolt(), 3)
 				.fieldsGrouping("MessageReceiverBolt", new Fields("companyId"));
-		builder.setBolt("DisplayBoltDevice", new DisplayBoltDevice(), 3)
+		builder.setBolt("UserDisplayBolt", new FileWriterBolt(), 3)
 				.shuffleGrouping("UserAggregatorBolt");
-		builder.setBolt("DisplayBoltCompany", new DisplayBoltCompany(), 3)
-				.shuffleGrouping("CompanyAggregatorBolt");
+		builder.setBolt("UserDBWriterBolt", new DBWriterBolt(), 1)
+				.shuffleGrouping("UserAggregatorBolt");
+		builder.setBolt("CompanyDBWriterBolt", new DBWriterBolt(), 1)
+				.shuffleGrouping("CompanyAggregateBolt");
 
 		Config conf = new Config();
 		conf.setDebug(false);
@@ -174,7 +68,5 @@ public class ActivizeTopology {
 		br.readLine();
 
 		cluster.shutdown();
-		bw.close();
-		bw2.close();
 	}
 }
--- storm-starter/src/storm/starter/bolt/DBWriterBolt.java
+++ storm-starter/src/storm/starter/bolt/DBWriterBolt.java
@@ -1,5 +1,94 @@
 package storm.starter.bolt;
 
-public class DBWriterBolt {
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
 
+import backtype.storm.topology.BasicOutputCollector;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseBasicBolt;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+
+public class DBWriterBolt extends BaseBasicBolt {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void execute(Tuple tuple, BasicOutputCollector collector) {
+
+		// Define the SQL string.
+		String sqlString = "INSERT INTO "
+				+ "[ActivizeDB].[dbo].[Monthly]([Metric],[Avg Values],[Timestamp],[Company]) "
+				+ "VALUES ('string',1234,'7/23/2014 11:04:00','Microsoft')";
+
+		loadPropertiesDB(sqlString);
+
+		collector.emit(new Values("DB"));
+	}
+
+	@Override
+	public void declareOutputFields(OutputFieldsDeclarer declarer) {
+		declarer.declare(new Fields("DBWriter"));
+	}
+
+	public static void connectDB(String connectionString, String sqlString) {
+
+		Connection connection = null; // For making the connection
+		Statement statement = null; // For the SQL statement
+		ResultSet resultSet = null; // For the result set
+
+		try {
+			// Ensure the SQL Server driver class is available.
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+			// Establish the connection.
+			connection = DriverManager.getConnection(connectionString);
+
+			// Use the connection to create the SQL statement.
+			statement = connection.createStatement();
+
+			// Execute the statement.
+			statement.executeUpdate(sqlString);
+
+		}
+		// Exception handling
+		catch (ClassNotFoundException cnfe) {
+			System.out.println("ClassNotFoundException " + cnfe.getMessage());
+		} catch (Exception e) {
+			System.out.println("Exception " + e.getMessage());
+			e.printStackTrace();
+		} finally {
+			try {
+				// Close resources.
+				if (null != connection)
+					connection.close();
+				if (null != statement)
+					statement.close();
+				if (null != resultSet)
+					resultSet.close();
+			} catch (SQLException sqlException) {
+				// No additional action if close() statements fail.
+			}
+		}
+
+	}
+
+	public static void loadPropertiesDB(String sqlString) {
+		Properties prop = new Properties();
+		try {
+			prop.load(DBWriterBolt.class.getClassLoader().getResourceAsStream(
+					"config.properties"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		String connectionString = prop.getProperty("SQL_CONNECTION");
+		
+		connectDB(connectionString, sqlString);
+	}
 }
